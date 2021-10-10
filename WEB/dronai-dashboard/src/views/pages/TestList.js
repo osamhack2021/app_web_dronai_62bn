7cd75a444b79fff1f42073ddf7860d6d91d7f075
2076049438 <quo1031@gmail.com>
2021-10-10 05:59:10 +0000

    expand cell renderer

--- WEB/dronai-dashboard/src/views/pages/TestList.js
+++ WEB/dronai-dashboard/src/views/pages/TestList.js
@@ -1,8 +1,13 @@
 import React, {useState, useEffect} from 'react';
 //import {Text} from 'react-native';
 // material-ui
+import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+
 import { DataGrid, useGridApiRef } from '@material-ui/data-grid';
-import {makeStyles} from '@material-ui/styles';
+import {createStyles, makeStyles} from '@material-ui/styles';
 
 import configData from '../../config';
 
@@ -19,6 +24,143 @@ import imgA from './../../assets/images/logo.png';
 
 //==============================|| TYPOGRAPHY ||==============================//
 
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      alignItems: 'center',
+      lineHeight: '24px',
+      width: '100%',
+      height: '100%',
+      position: 'relative',
+      display: 'flex',
+      '& .cellValue': {
+        whiteSpace: 'nowrap',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+      },
+    },
+  }),
+);
+
+function isOverflown(element) {
+  return (
+    element.scrollHeight > element.clientHeight ||
+    element.scrollWidth > element.clientWidth
+  );
+}
+
+const GridCellExpand = React.memo(function GridCellExpand(props) {
+  const { width, value } = props;
+  const wrapper = React.useRef(null);
+  const cellDiv = React.useRef(null);
+  const cellValue = React.useRef(null);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const classes = useStyles();
+  const [showFullCell, setShowFullCell] = React.useState(false);
+  const [showPopper, setShowPopper] = React.useState(false);
+
+  const handleMouseEnter = () => {
+    const isCurrentlyOverflown = isOverflown(cellValue.current);
+    setShowPopper(isCurrentlyOverflown);
+    setAnchorEl(cellDiv.current);
+    setShowFullCell(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowFullCell(false);
+  };
+
+  React.useEffect(() => {
+    if (!showFullCell) {
+      return undefined;
+    }
+
+    function handleKeyDown(nativeEvent) {
+      // IE11, Edge (prior to using Bink?) use 'Esc'
+      if (nativeEvent.key === 'Escape' || nativeEvent.key === 'Esc') {
+        setShowFullCell(false);
+      }
+    }
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [setShowFullCell, showFullCell]);
+
+  return (
+    <div
+      ref={wrapper}
+      className={classes.root}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div
+        ref={cellDiv}
+        style={{
+          height: 1,
+          width,
+          display: 'block',
+          position: 'absolute',
+          top: 0,
+        }}
+      />
+      <div ref={cellValue} className="cellValue">
+        {value}
+      </div>
+      {showPopper && (
+        <Popper
+          open={showFullCell && anchorEl !== null}
+          anchorEl={anchorEl}
+          style={{ width, marginLeft: -17 }}
+        >
+          <Paper
+            elevation={1}
+            style={{ minHeight: wrapper.current.offsetHeight - 3 }}
+          >
+            <Typography variant="body2" style={{ padding: 8 }}>
+              {value}
+            </Typography>
+          </Paper>
+        </Popper>
+      )}
+    </div>
+  );
+});
+
+GridCellExpand.propTypes = {
+  value: PropTypes.string.isRequired,
+  width: PropTypes.number.isRequired,
+};
+
+function renderCellExpand(params) {
+  return (
+    <GridCellExpand
+      value={params.value ? params.value.toString() : ''}
+      width={params.colDef.computedWidth}
+    />
+  );
+}
+
+renderCellExpand.propTypes = {
+  /**
+   * The column of the row that the current cell belongs to.
+   */
+  colDef: PropTypes.object.isRequired,
+  /**
+   * The cell value, but if the column has valueGetter, use getValue.
+   */
+  value: PropTypes.oneOfType([
+    PropTypes.instanceOf(Date),
+    PropTypes.number,
+    PropTypes.object,
+    PropTypes.string,
+    PropTypes.bool,
+  ]),
+};
+
+
 const columns = [
     {
         field: 'imgPath', headerName: 'Image', width: 300
@@ -29,15 +171,12 @@ const columns = [
             //<img src="/static/media/logo.011a3aea.png"></img>
         )
     },
-    { field: 'date', headerName: 'Date', width: 100 },
-    { field: 'droneId', headerName: 'DRONE ID', width: 170 },
-    { field: 'id', headerName: 'ID', width: 100 },
+    { field: 'date', headerName: 'Date', width: 100, renderCell: renderCellExpand, },
+    { field: 'droneId', headerName: 'DRONE ID', width: 170, renderCell: renderCellExpand, },
+    { field: 'id', headerName: 'ID', width: 100, renderCell: renderCellExpand, },
     {
         field: 'detail', headerName: 'Detail', width: 250
-        , renderCell: (params) => (
-            `${params.value}`
-
-        )
+         , renderCell: renderCellExpand,
     }
 ]
 
@@ -56,12 +195,6 @@ const rows = [
     { imgPath: 'i', id: 23, detail: 'test3' },
     { imgPath: '/static/media/profile.664db376.jpg', id: 5, detail: 'test4' }
 ]
-
-const useStyles = makeStyles({
-    text: {
-        wordBreak : 'break-all'
-    }
-})
 
 const TestList = () => {
     var loading;
